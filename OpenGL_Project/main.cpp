35374abaaf54d60cecd4c59d8395a29f3550c2a4
jjudy_kim <imjubaaakim@gmail.com>
2021-07-23 17:01:13 +0900

    Program 2.1 First C++/OpenGL Application

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -17,7 +17,7 @@ int main()
 		if(!glfwInit()) exit(EXIT_FAILURE);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-		GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+		GLFWwindow *window = glfwCreateWindow(400, 600, "Chapter2 - program1", NULL, NULL);
 		glfwMakeContextCurrent(window);
 		if(glewInit() != GLEW_OK) exit(EXIT_FAILURE);
 		glfwSwapInterval(1);
